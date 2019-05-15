57717eb53aeb8f7f675cf1fab57c7550ac2ec1eb
Tony <h8130084@live.tees.ac.uk>
2019-05-15 17:05:44 +0000

    amended typos to get the github api showing users

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -8,7 +8,7 @@ function userInformationHTML(user) {
     <div class="gh-content">
     <div class="gh-avatar">
     <a href="${user.html_url}" target="_blank">
-       <img src="${user.avatar_url}"width="80" height="80" alt="${user.login}" />
+       <img src="${user.avatar_url}" width="80" height="80" alt="${user.login}" />
        </a>
        </div>
        <p>Followers: ${user.followers} - Following ${user.following} <br> Repos: ${user.public_repos}</p>
@@ -16,7 +16,7 @@ function userInformationHTML(user) {
 }
 
 function repoInformationHTML(repos) {
-    if(repos.length === 0) {
+    if (repos.length === 0) {
         return `<div class="clearfix repo-list">No repos!</div>`;
     }
     
@@ -59,19 +59,19 @@ function fetchGitHubInformation(event) {
             function(response, secondResponse) {
                 var userData = response[0];
                 var repoData = secondResponse[0];
-                $("gh-user-data").html(userInformationHTML(userData));
-                $("gh-user-data").html(repoInformationHTML(repoData));
-            }, function(errorReponse) {
-                if (errorReponse.status === 404) {
-                    $("gh-user-data").html(
+                $("#gh-user-data").html(userInformationHTML(userData));
+                $("#gh-repo-data").html(repoInformationHTML(repoData));
+            }, function(errorResponse) {
+                if (errorResponse.status === 404) {
+                    $("#gh-user-data").html(
                         `<h2>No info found for user ${username}</h2>`);
-                } else if(errorReponse.status === 403) {
-                    var resetTime = new Date(errorReponse.getResponseHeader('X-RateLimit-Reset')*1000);
+                } else if(errorResponse.status === 403) {
+                    var resetTime = new Date(errorResponse.getResponseHeader('X-RateLimit-Reset')*1000);
                     $("#gh-user-data").html(`<h4>Too many requests, please wait until ${resetTime.toLocaleTimeString()}</h4>`);
                 } else {
-                    console.log(errorReponse);
+                    console.log(errorResponse);
                     $("#gh-user-data").html(
-                            `<h2>Error: ${errorReponse.responseJSON.message}</h2>`);
+                            `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
             }
         });
 }
